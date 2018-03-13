ea52855b21fd3568ddd689104ca063dde7494420
Seth Feder <sethfeder2017@u.northwestern.edu>
2018-03-13 14:23:41 -0500

    2nd commit

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheese');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Bacon Ranch');
+INSERT INTO burgers (burger_name) VALUES ('Pear Gorganzola');
+INSERT INTO burgers (burger_name) VALUES ('Impossible');
+INSERT INTO burgers (burger_name) VALUES ('Hangover');
+INSERT INTO burgers (burger_name) VALUES ('Au Cheval');
